1018f07b09999efcabb2d8403a9cf525d3beddf0
flaviomessina <47560705+flaviomessina@users.noreply.github.com>
2023-03-22 11:23:18 +0000

    summary

--- backgroupSearch.js
+++ backgroupSearch.js
@@ -1,3 +1,29 @@
+self.addEventListener('message', function(e) {
+
+  	// code to be run
+
+	let datatableId = "13a448c9-8c47-4ddd-992d-8aa2d3add55e"; // String | id of datatable
+	let dataTableRow = null; // {String: Object} | 
+	  let searchId = Date.now();
+	  let searchTerm = "hi there";
+
+	  console.log(searchId)
+	  console.log(searchTerm)
+
+	  dataTableRow = '{"search":"'+ searchTerm +'", "key": "'+ searchId +'"}';
+	  console.log(dataTableRow);
+
+	  // Create a new row entry for the datatable.
+	  //architectApi = new platformClient.ArchitectApi();
+	  architectApi.postFlowsDatatableRows(datatableId, dataTableRow)
+	    .then((data) => {
+	      console.log(`postFlowsDatatableRows success! data: ${JSON.stringify(data, null, 2)}`);
+	    })
+	    .catch((err) => {
+	      console.log("There was a failure calling postFlowsDatatableRows");
+	      console.error(err);
+	    });
+
 	    var message = e.data + 'to myself!';
 	    self.postMessage(message);
 		self.close();
